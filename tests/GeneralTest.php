339a513837e2fc751d03f39293b57cdea05bf3df
lorenzop <lorenzo@poixson.com>
2015-06-24 22:28:41 -0400

    added tests for General class testing [] empty array testing GCSupport() InstanceOfClass() and ValidateClass() functions

--- tests/GeneralTest.php
+++ tests/GeneralTest.php
@@ -18,6 +18,12 @@ class GeneralTest extends \PHPUnit_Framework_TestCase {
 
 
 
+	public function testArray() {
+		$this->assertEmpty([]);
+	}
+
+
+
 	public function testTimestamp() {
 		// all timings are in ms
 		$this->PerformTimestampTest(
@@ -41,6 +47,129 @@ class GeneralTest extends \PHPUnit_Framework_TestCase {
 		// within 5-15ms
 		$this->assertGreaterThan($minExpected / 1000, $c);
 		$this->assertLessThan(   $maxExpected / 1000, $c);
+
+
+
+	/**
+	 * @covers ::GDSupported
+	 */
+	public function testGDSupported() {
+		$this->assertEquals(
+				\function_exists('imagepng'),
+				General::GDSupported()
+		);
+	}
+
+
+
+	const VALIDATE_CLASS_STRING = 'pxn\\phpUtils\\tests\\GeneralTest';
+
+
+
+	public function testClassName() {
+		$this->assertEquals(
+				self::VALIDATE_CLASS_STRING,
+				\get_class($this)
+		);
+	}
+
+
+
+	/**
+	 * @covers ::InstanceOfClass
+	 */
+	public function testInstanceOfClass() {
+		$this->assertTrue(
+				General::InstanceOfClass(
+						self::VALIDATE_CLASS_STRING,
+						$this
+				)
+		);
+	}
+
+
+
+	/**
+	 * @covers ::ValidateClass
+	 */
+	public function testValidateClass() {
+		General::ValidateClass(
+				self::VALIDATE_CLASS_STRING,
+				$this
+		);
+	}
+	/**
+	 * @covers ::ValidateClass
+	 */
+	public function testValidateClass_NullString() {
+		try {
+			General::ValidateClass(
+					NULL,
+					$this
+			);
+		} catch (\InvalidArgumentException $e) {
+			$this->assertEquals(
+					'classname not defined',
+					$e->getMessage()
+			);
+			return;
+		}
+		$this->assertTrue(FALSE, 'Failed to throw expected exception');
+	}
+	/**
+	 * @covers ::ValidateClass
+	 */
+	public function testValidateClass_BlankString() {
+		try {
+			General::ValidateClass(
+					'',
+					$this
+			);
+		} catch (\InvalidArgumentException $e) {
+			$this->assertEquals(
+					'classname not defined',
+					$e->getMessage()
+			);
+			return;
+		}
+		$this->assertTrue(FALSE, 'Failed to throw expected exception');
+	}
+	/**
+	 * @covers ::ValidateClass
+	 */
+	public function testValidateClass_NullObject() {
+		try {
+			General::ValidateClass(
+					self::VALIDATE_CLASS_STRING,
+					NULL
+			);
+		} catch (\InvalidArgumentException $e) {
+			$this->assertEquals(
+					'object not defined',
+					$e->getMessage()
+			);
+			return;
+		}
+		$this->assertTrue(FALSE, 'Failed to throw expected exception');
+	}
+	/**
+	 * @covers ::ValidateClass
+	 */
+	public function testValidateClass_ClassType() {
+		try {
+			General::ValidateClass(
+					self::VALIDATE_CLASS_STRING.'_invalid',
+					$this
+			);
+		} catch (\InvalidArgumentException $e) {
+			$this->assertEquals(
+					'Class object isn\'t of type '.
+						self::VALIDATE_CLASS_STRING.'_invalid',
+					$e->getMessage()
+			);
+			return;
+		}
+		$this->assertTrue(FALSE, 'Failed to throw expected exception');
 	}
 
 
