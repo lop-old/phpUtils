bb33639ad5eee148506c1e81bf91b555ca95b3d7
lorenzop <lorenzo@poixson.com>
2015-07-16 21:21:28 -0400

    added more tests for ComposerTools class

--- tests/ComposerTest.php
+++ tests/ComposerTest.php
@@ -22,11 +22,47 @@ class ComposerTest extends \PHPUnit_Framework_TestCase {
 	/**
 	 * @covers ::get
 	 * @covers ::__construct
+	 * @covers ::getPath
+	 */
+	public function testPaths() {
+		$expect = \realpath(__DIR__.'/../composer.json');
+		// default path
+		$composer = ComposerTools::get();
+		$this->assertNotNull($composer);
+		$this->assertEquals($expect, $composer->getFilePath());
+		unset($composer);
+		// exact path
+		$path = \realpath(__DIR__.'/../');
+		$composer = ComposerTools::get($path.'/composer.json');
+		$this->assertNotNull($composer);
+		$this->assertEquals($expect, $composer->getFilePath());
+		unset($composer, $path);
+		// invalid path
+		try {
+			$composer = ComposerTools::get('notexisting');
+			$this->assertFalse(TRUE, 'Expected exception not thrown!');
+			return;
+		} catch (\Exception $ignore) {}
+	}
+
+
+
+	public function testInstances() {
+		$a = ComposerTools::get();
+		$b = ComposerTools::get();
+		$this->assertTrue($a === $b);
+	}
+
+
+
+	/**
+	 * @covers ::get
+	 * @covers ::__construct
 	 * @covers ::getVersion
 	 * @covers ::getHomepage
 	 */
 	public function testValues() {
-		$composer = ComposerTools::get(__DIR__.'/../');
+		$composer = ComposerTools::get();
 		$this->assertNotNull($composer);
 		// version
 		$version = $composer->getVersion();
