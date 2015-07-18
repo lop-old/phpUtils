c470b8ebf7035644467814b05103c12c1e443fe8
lorenzop <lorenzo@poixson.com>
2015-07-18 10:00:38 -0400

    added FileNotFoundException

---
+++ src/Exceptions/FileNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * PoiXson phpUtils - Utilities for PoiXson PHP projects
+ *
+ * @copyright 2004-2015
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\phpUtils\Exceptions;
+
+class FileNotFoundException extends \Exception {
+
+}
