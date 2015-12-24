8e0b596c57781033e8bea5ff1d6dd45625098948
lorenzop <lorenzo@poixson.com>
2015-12-24 18:42:48 -0500

    small fixes to comment headers

--- tests/ArraysTest.php
+++ tests/ArraysTest.php
@@ -1,12 +1,11 @@
 <?php
 /*
  * PoiXson phpUtils - Website Utilities Library
-*
-* @copyright 2004-2015
-* @license GPL-3
-* @author lorenzo at poixson.com
-* @link http://poixson.com/
-*/
+ * @copyright 2004-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
 namespace pxn\phpUtils\tests;
 
 use pxn\phpUtils\Arrays;
--- tests/DefinesTest.php
+++ tests/DefinesTest.php
@@ -1,8 +1,7 @@
 <?php
 /*
  * PoiXson phpUtils - Website Utilities Library
- *
- * @copyright 2004-2015
+ * @copyright 2004-2016
  * @license GPL-3
  * @author lorenzo at poixson.com
  * @link http://poixson.com/
