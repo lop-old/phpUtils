f592fea16307e3830250c3deed5705c8f1bbcd29
lorenzop <lorenzo@poixson.com>
2015-12-25 12:02:19 -0500

    added Paths class (mostly finished)

---
+++ src/Paths.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * PoiXson phpUtils - Website Utilities Library
+ * @copyright 2004-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\phpUtils;
+
+
+final class Paths {
+	private function __construct() {}
+
+	// local paths
+	protected static $local_entry = '';
+	protected static $local_base  = '';
+	protected static $local_site  = '';
+
+	// web paths
+	protected static $web_base   = '';
+	protected static $web_images = '';
+
+
+
+	public static function init() {
+		// local paths
+//		$paths['local']['entry'] = $_SERVER['DOCUMENT_ROOT'];
+		self::$local_entry = \getcwd();
+		self::$local_base  = __DIR__;
+		// web paths
+		self::$web_images = '/static';
+	}
+
+
+
+	public static function entry() {
+		return self::$local_entry;
+	}
+	public static function base() {
+		return self::$local_base;
+	}
+	public static function site() {
+		// find site path
+		if(empty(self::$local_site)) {
+//self::$site = self::entry().DIR_SEP.
+//  \psm\portal::get()->getWebsite()->siteName();
+			$website = \pxn\phpUtils\portal\Website::peak();
+			// website object not loaded yet
+			if($website === NULL) {
+//TODO:
+				echo '<p>Website object not loaded yet!</p>'.\CRLF;
+				exit(1);
+			}
+				return NULL;
+				
+//			$path = 
+			$path = self::entry().\DIR_SEP.
+				$website->getName();
+				
+			unset($path);
+		}
+		return self::$local_site;
+	}
+
+
+
+}
