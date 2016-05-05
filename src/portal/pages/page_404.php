ce75920ea39590528257140860fc549e7313b2e4
lorenzop <lorenzo@poixson.com>
2016-05-05 16:16:19 -0400

    finished $FailedPage value in page_404

--- src/portal/pages/page_404.php
+++ src/portal/pages/page_404.php
@@ -8,18 +8,22 @@
  */
 namespace pxn\phpUtils\portal\pages;
 
+use pxn\phpUtils\portal\Website;
+use pxn\phpUtils\Defines;
+
 
 class page_404 {
 
 
 
 	public function getPageContents() {
+		$FailedPage = Website::get()->getArg(Defines::KEY_FAILED_PAGE);
 		return
-'<center>
+"<center>
 	<h1>404 - Page Not Found!</h1>
-	<h3>{{FailedPage}}</h3>
+	<h3>{$FailedPage}</h3>
 </center>
-';
+";
 	}
 
 
