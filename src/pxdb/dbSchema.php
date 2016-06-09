9f93a13f2e5afed58889b8a86a1a631c7770f294
lorenzop <lorenzo@poixson.com>
2016-06-08 23:37:00 -0400

    added dbSchema interface

---
+++ src/pxdb/dbSchema.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * PoiXson phpUtils - PHP Utilities Library
+ * @copyright 2004-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\phpUtils\pxdb;
+
+
+interface dbSchema {
+
+
+	public function getFields();
+
+
+}
