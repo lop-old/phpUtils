fb067fd45f3332de6f1399f6acee180a81fd4bac
lorenzop <lorenzo@poixson.com>
2016-08-19 00:03:36 -0400

    added Render and ShellRender classes

---
+++ src/app/Render.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * PoiXson phpUtils - PHP Utilities Library
+ * @copyright 2004-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\phpUtils\app;
+
+
+abstract class Render {
+
+
+
+
+
+
+
+
+}
---
+++ src/app/ShellRender.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * PoiXson phpUtils - PHP Utilities Library
+ * @copyright 2004-2016
+ * @license GPL-3
+ * @author lorenzo at poixson.com
+ * @link http://poixson.com/
+ */
+namespace pxn\phpUtils\app;
+
+
+class ShellRender extends Render {
+
+
+
+
+
+
+
+
+
+
+}
