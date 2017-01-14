72a30f9c4f5783bb4597ebd0ba4fee26295cd2a8
lorenzop <lorenzo@poixson.com>
2017-01-14 01:37:20 -0500

    added listing table fields to dbCommand_List class

--- src/pxdb/dbCommand_List.php
+++ src/pxdb/dbCommand_List.php
@@ -8,6 +8,8 @@
  */
 namespace pxn\phpUtils\pxdb;
 
+use pxn\phpUtils\Strings;
+
 
 class dbCommand_List extends dbCommands {
 
@@ -16,8 +18,27 @@ class dbCommand_List extends dbCommands {
 	public function execute($pool, $table) {
 		$poolName = dbPool::castPoolName($pool);
 		$tableExists = $pool->hasTable($table);
+		// found table
 		if ($tableExists) {
-			echo "Found:   {$poolName}:{$table}\n";
+			$msg = "Found:   {$poolName}:{$table}";
+			$msg = Strings::PadLeft($msg, 30, ' ');
+			$fields = $pool->getTableFields($table);
+			$count = count($fields);
+			$msg .= "[$count]";
+			// list the fields
+			if ($count > 0) {
+				$msg .= ' ';
+				$index = 0;
+				foreach ($fields as $fieldName => $field) {
+					if ($index++ > 0) {
+						$msg .= ', ';
+					}
+					$fieldType = $field['type'];
+					$msg .= "{$fieldType}|{$fieldName}";
+				}
+			}
+			echo "$msg\n";
+		// missing table
 		} else {
 			echo "Missing: {$poolName}:{$table}\n";
 		}
